03c372dec2efe2dfdb306c9d82f6d8e2841e17ca
gram15inch <leapthestairs@gmail.com>
2022-01-08 19:03:44 +0900

    dh

--- src/koInAc/ch02.kt
+++ src/koInAc/ch02.kt
@@ -1,11 +1,9 @@
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.lang.Exception
-
-
+package koInAc
+import koInAc.Color.*
 
 // 78p - 1/8
 enum class Color(
-    val r:Int, val g : Int, val b :Int
+    private val r:Int, private val g : Int, private val b :Int
 ){
     RED(255,0,0),ORANGE(255,165,0),
     YELLOW(255,255,0),GREEN(0,255,0)
@@ -13,31 +11,31 @@ enum class Color(
     fun rgb()=(r*256+g)+b
 }
 fun getMnemonic(c:Color)= when(c){
-        Color.RED ->"R"
-        Color.ORANGE ->"O"
-        Color.YELLOW ->"Y"
-        Color.GREEN ->"G"
-        Color.BLUE ->"B"
+        RED ->"R"
+        ORANGE ->"O"
+        YELLOW ->"Y"
+        GREEN ->"G"
+        BLUE ->"B"
     }
 // 객체를 스트링으로
 fun getWarmth(c: Color)= when(c){
-    Color.RED,Color.ORANGE,Color.YELLOW->"warm"
-    Color.GREEN->"neutral"
-    Color.BLUE->"cold"
+    RED, ORANGE,YELLOW->"warm"
+    GREEN ->"neutral"
+    BLUE->"cold"
 }
 // 객체조합을 객체로
 fun mix(c1:Color,c2:Color)= when(setOf(c1,c2)){
-    setOf(Color.RED,Color.YELLOW) -> Color.ORANGE
-    setOf(Color.BLUE,Color.YELLOW) -> Color.GREEN
+    setOf(RED,YELLOW) -> ORANGE
+    setOf(BLUE,YELLOW) -> GREEN
     else-> throw Exception("Dirty color")
 }
 // 객체리스트를 부울로
 fun mt(c1: Color,c2:Color):Boolean{
-    val M1= mutableListOf(Color.RED,Color.GREEN)
-    val M2= mutableListOf(Color.RED,Color.YELLOW)
-    val M3= mutableListOf(Color.RED,Color.BLUE)
+    val m1= mutableListOf(RED,GREEN)
+    val m2= mutableListOf(RED,YELLOW)
+    val m3= mutableListOf(RED,BLUE)
     return when(mutableListOf(c1,c2)){
-        mutableListOf(Color.BLUE,Color.GREEN),M2,M3-> true
+        mutableListOf(BLUE,GREEN),m2,m3-> true
         else -> false
     }
 }
@@ -50,7 +48,15 @@ fun errorCodeDescription(ec:ErrorCode) = when(ec){
     else-> throw Exception("notCode")
 }
 
+fun mixOptimized(c1:Color,c2: Color)= when
+{
+    (c1 == RED && c2 == YELLOW) || (c1 == RED && c2 == BLUE)-> ORANGE
+    (c1 == RED && c2 == GREEN) ->  BLUE
+    else -> throw java.lang.Exception("Dirty Color")
+}
+
 
 fun main() {
+    println(mixOptimized(RED,YELLOW))
 
 }
