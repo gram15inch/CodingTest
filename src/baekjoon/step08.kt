a190d707e6d290b43ac396dc96bc7528f2e5faf6
gram15inch <94951889+gram15inch@users.noreply.github.com>
2022-01-18 01:53:11 +0900

    Update step08.kt

--- src/baekjoon/step08.kt
+++ src/baekjoon/step08.kt
@@ -31,23 +31,16 @@ fun step0802(){
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val a :Int
-    val b :Int
-    val c :Int
+    var n =0
+    val num = br.readLine().toInt()
+    var an = 0
 
-    var token = StringTokenizer(br.readLine())
-    //var token = StringTokenizer("3 2 1")
-
-    a = token.nextToken().toInt()
-    b = token.nextToken().toInt()
-    c = token.nextToken().toInt()
-
-
-    if(b>=c){
-        bw.write("-1")
+    while(an < num) {
+        n++
+        an = 3*n*(n-1)+1
     }
-    else
-        bw.write("${a /(c  -  b) +1}")
+
+    bw.write("$n")
 
     bw.flush()
     bw.close()
