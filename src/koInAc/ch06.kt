266652bf40935d8a452e823d73db8880e7a5b872
gram15inch <leapthestairs@gmail.com>
2022-01-30 19:08:22 +0900

    1/30

--- src/koInAc/ch06.kt
+++ src/koInAc/ch06.kt
@@ -1,6 +1,8 @@
 package koInAc
 
+import javafx.scene.control.Button
 import java.io.File
+import java.util.concurrent.Delayed
 
 // 243p
 
@@ -48,12 +50,31 @@ fun createSeq(){
     isInsideHiddenLocal(file)
 
     /* 궁금한 사항
-        1. generateSequence() 는 왜 괄호뒤에 람다가 나오는지
+        1. generateSequence() 는 왜 괄호뒤에 람다가 나오는지 - 해결
         2. 괄호 안에는 list 는 들어갈수 없는지
-        3. list 가 들어간다면     
+        3. list 가 들어간다면 중괄호에서 원소는 어떻게 꺼내는지
+        4. 2에서 왜 첫번째 중괄호뒤에 filter 나 map 을 쓸수 없는지
      */
 }
+fun postponeComputation(delay :Int, computation:Runnable){
+    computation.run()
+}
+fun javaApiInKotlin(){
+// 객체를 명시적으로 선언해 전달
+    postponeComputation(1000, object :Runnable{ // 메소드 호출시마다 객체 생성
+        override fun run() =println(42)
+    })
+// 매개변수를 람다로 전달
+    postponeComputation(1000){ println(42) } // 람다를 한번만 무명객체에 생성하고 호출시마다 사용
+    val runnable = Runnable { println(42) } // 이 코드와 같음
+    postponeComputation(1000, runnable)
 
+// 람다가 주변 영역의 변수를 포획
+    fun handleComputation(id:String){
+        postponeComputation(1000){println(id)} // 매번 포획한 변수를 포핳한 새 객체를 생성
+    }
+}
+// 233p
 fun main() {
-    mapFilterOrder()
+
 }
