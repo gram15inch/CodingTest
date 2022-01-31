8db6232fca22aec15784341ea1dd90444f057758
gram15inch <leapthestairs@gmail.com>
2022-01-31 19:44:09 +0900

    1//31

--- src/koInAc/ch05.kt
+++ src/koInAc/ch05.kt
@@ -1,5 +1,7 @@
 package koInAc
 
+import java.io.File
+
 
 // 197p - 1/26
 
@@ -199,7 +201,7 @@ fun findAge(){
 }
 fun groupByAge(){
     val people = listOf(Person("Alice",29), Person("Bob",31), Person("Carol",31))
-    // groupBy
+// groupBy
     val m : Map<Int, List<Person>> = people.groupBy { it.age } // 조건에 맞는 원소들모아 특성과 함께 map 형태로 집어넣음
 }
 
@@ -229,4 +231,50 @@ fun flatMapFlatten(){
 }
 
 
+// 223p - 1/30
+// 지연 계산(lazy) 컬렉션 연산
+fun sequenceLazy(){
+    val people = listOf(Person("Alice",29), Person("Bob",31), Person("Carol",31))
+    people.map(Person::name).filter{it.startsWith("A")} // map, filter 단계마다 임시 컬렉션 생성 :: 비효율
+// asSequence
+    val m =people.asSequence() // 중간컬렉션이 생기지 않음
+        .map(Person::name)
+        .filter{ it.startsWith("A") }
+        .toList() // sequence 는 연산과정의 나열일뿐 list 변환해야 결과값을 얻을수 있음
+// lazy
+    val n = listOf(1,2,3,4).asSequence() // 모든 원소에 대해 각각 map, find 연산을 한다
+        .map{ it*it } // 2*2
+        .find{ it>3 } // 2에서 결과가 나왔음으로 이후 연산을 하지않음
+}
+// map 과 filter 의 순서에 따른 비용 차이
+fun mapFilterOrder(){
+    val people = listOf(Person("Alice",29), Person("Bob",31), Person("Carol",31), Person("Dan",21))
 
+    people.asSequence()
+        .map(Person::name) // 조건에 상관없이 map 연산이 실행 : 비효율
+        .filter { it.length>4 }
+    people.asSequence()
+        .filter { it.name.length>4 } // 먼저 필터를 한후 map 실행
+        .map(Person::name)
+}
+// 자연수의 시퀀스를 생성하고 사용하기 - 228p
+fun createSeq(){
+    val naturalNumber = generateSequence(0){it +1}
+    val numbersTo100 = naturalNumber.takeWhile { it<=100 }
+    val sum = numbersTo100.sum()
+
+    // 상위 디렉터리의 시퀀스를 생성하고 사용하기
+    fun File.isInsideHiddenDirectory()= generateSequence(this){it.parentFile}.any{it.isHidden}
+    val file = File("/Users/svtk/.HiddenDir/a.txt")
+    file.isInsideHiddenDirectory()
+
+    fun isInsideHiddenLocal(file: File)= generateSequence(file){it.parentFile}.any{it.isHidden}
+    isInsideHiddenLocal(file)
+
+    /* 궁금한 사항
+        1. generateSequence() 는 왜 괄호뒤에 람다가 나오는지 - 해결
+        2. 괄호 안에는 list 는 들어갈수 없는지
+        3. list 가 들어간다면 중괄호에서 원소는 어떻게 꺼내는지
+        4. 2에서 왜 첫번째 중괄호뒤에 filter 나 map 을 쓸수 없는지
+     */
+}
--- src/koInAc/ch06.kt
+++ src/koInAc/ch06.kt
@@ -8,55 +8,9 @@ import java.util.concurrent.Delayed
 
 // ch05로 가야함
 
-// 223p - 1/30
-// 지연 계산(lazy) 컬렉션 연산
-fun sequenceLazy(){
-    val people = listOf(Person("Alice",29), Person("Bob",31), Person("Carol",31))
-    people.map(Person::name).filter{it.startsWith("A")} // map, filter 단계마다 임시 컬렉션 생성 :: 비효율
-// asSequence
-    val m =people.asSequence() // 중간컬렉션이 생기지 않음
-        .map(Person::name)
-        .filter{ it.startsWith("A") }
-        .toList() // sequence 는 연산과정의 나열일뿐 list 변환해야 결과값을 얻을수 있음
-// lazy
-    val n = listOf(1,2,3,4).asSequence() // 모든 원소에 대해 각각 map, find 연산을 한다
-        .map{ it*it } // 2*2
-        .find{ it>3 } // 2에서 결과가 나왔음으로 이후 연산을 하지않음
-}
-// map 과 filter 의 순서에 따른 비용 차이
-fun mapFilterOrder(){
-    val people = listOf(Person("Alice",29), Person("Bob",31), Person("Carol",31),Person("Dan",21))
-
-    people.asSequence()
-        .map(Person::name) // 조건에 상관없이 map 연산이 실행 : 비효율
-        .filter { it.length>4 }
-    people.asSequence()
-        .filter { it.name.length>4 } // 먼저 필터를 한후 참일때만 map 실행
-        .map(Person::name)
-
-}
-// 자연수의 시퀀스를 생성하고 사용하기 - 228p
-fun createSeq(){
-    val naturalNumber = generateSequence(0){it +1}
-    val numbersTo100 = naturalNumber.takeWhile { it<=100 }
-    val sum = numbersTo100.sum()
-
-// 상위 디렉터리의 시퀀스를 생성하고 사용하기
-    fun File.isInsideHiddenDirectory()= generateSequence(this){it.parentFile}.any{it.isHidden}
-    val file = File("/Users/svtk/.HiddenDir/a.txt")
-    file.isInsideHiddenDirectory()
-
-    fun isInsideHiddenLocal(file:File)= generateSequence(file){it.parentFile}.any{it.isHidden}
-    isInsideHiddenLocal(file)
-
-    /* 궁금한 사항
-        1. generateSequence() 는 왜 괄호뒤에 람다가 나오는지 - 해결
-        2. 괄호 안에는 list 는 들어갈수 없는지
-        3. list 가 들어간다면 중괄호에서 원소는 어떻게 꺼내는지
-        4. 2에서 왜 첫번째 중괄호뒤에 filter 나 map 을 쓸수 없는지
-     */
-}
-fun postponeComputation(delay :Int, computation:Runnable){
+// 1/31
+fun postponeComputation(delay :Int, computation:Runnable) {
+    println("$delay")
     computation.run()
 }
 fun javaApiInKotlin(){
@@ -75,6 +29,8 @@ fun javaApiInKotlin(){
     }
 }
 // 233p
+
+
 fun main() {
 
 }
